c3457b599878a318fa4f9c9c733dc867fa125860
tgybyrl <tgybyrl48@gmail.com>
2026-05-16 14:36:27 +0300

    Saved corrupted files but lost previous commits but here we have Asteroids game with ship and asteroids, shooting and collisions on next update

---
+++ main.py
@@ -0,0 +1,44 @@
+import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
+
+# uv run main.py
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    asteroids = pygame.sprite.Group()
+    updatable = pygame.sprite.Group() 
+    drawable = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    AsteroidField()
+    Player.containers = (updatable, drawable) 
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    clock = pygame.time.Clock()
+    dt = 0
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return 
+
+        screen.fill("black")
+        
+        for draws in drawable:
+            draws.draw(screen)
+        pygame.display.flip()
+        updatable.update(dt)
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+    
+
+if __name__ == "__main__":
+    main()
