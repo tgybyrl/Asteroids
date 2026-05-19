a53180e222a12e234b0b4ac1b32e1027512a2cd3
tgybyrl <tgybyrl48@gmail.com>
2026-05-19 16:43:29 +0300

    Cooldown timer added to bullets

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, SHOT_RADIUS, PLAYER_SHOOT_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, SHOT_RADIUS, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN_SECONDS
 from shot import Shot
 
 
@@ -8,6 +8,7 @@ class Player(CircleShape):
   def __init__(self, x, y):
     super().__init__(x, y, PLAYER_RADIUS)
     self.rotation = 0
+    self.cooldown_timer = 0
     
 
   # in the Player class
@@ -25,6 +26,7 @@ class Player(CircleShape):
 
   def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.cooldown_timer -= dt
 
         if keys[pygame.K_a]:
           self.rotate(-dt)
@@ -36,7 +38,11 @@ class Player(CircleShape):
           self.move(-dt)
 
         if keys[pygame.K_SPACE]:
-          self.shoot()
+          if self.cooldown_timer > 0:
+            pass
+          else:
+            self.cooldown_timer = PLAYER_SHOOT_COOLDOWN_SECONDS
+            self.shoot()
 
   def move(self, dt):
     unit_vector = pygame.Vector2(0, 1)
