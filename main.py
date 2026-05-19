78abfb1ed0dfff4d123fd60a2fb020e65e7ba76e
tgybyrl <tgybyrl48@gmail.com>
2026-05-19 16:51:13 +0300

    bullets collides with asteroids.

--- main.py
+++ main.py
@@ -45,6 +45,14 @@ def main():
                 print("Game over!")
                 sys.exit()
 
+        for asteroid in asteroids:
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    pygame.sprite.Sprite.kill(asteroid)
+                    pygame.sprite.Sprite.kill(shot)
+
+
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
     
