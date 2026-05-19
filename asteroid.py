488ec1612fb5935c400d105e67e0e9bab24351ea
tgybyrl <tgybyrl48@gmail.com>
2026-05-19 18:16:24 +0300

    Asteroids now splits when shot

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,9 @@
 import pygame
+from logger import log_event
+import random
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
+
 
 class Asteroid(CircleShape):
   def __init__(self, x, y, radius):
@@ -12,3 +15,18 @@ class Asteroid(CircleShape):
 
   def update(self, dt):
     self.position += self.velocity * dt
+
+  def split(self):
+    pygame.sprite.Sprite.kill(self)
+    if self.radius <= ASTEROID_MIN_RADIUS:
+      return
+    else:
+      log_event("asteroid_split")
+      random_angle = random.uniform(20, 50)
+      rotated_velocity_with_angle = self.velocity.rotate(random_angle)
+      opposite_rotated_velocity_with_angle = self.velocity.rotate(-random_angle)
+      new_radius = self.radius - ASTEROID_MIN_RADIUS
+      asteroid_1 = Asteroid(self.position.x, self.position.y, new_radius)
+      asteroid_2 = Asteroid(self.position.x, self.position.y, new_radius)
+      asteroid_1.velocity = rotated_velocity_with_angle * 1.5
+      asteroid_2.velocity = opposite_rotated_velocity_with_angle * 1.5
--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def main():
             for shot in shots:
                 if shot.collides_with(asteroid):
                     log_event("asteroid_shot")
-                    pygame.sprite.Sprite.kill(asteroid)
+                    asteroid.split()
                     pygame.sprite.Sprite.kill(shot)
 
 
